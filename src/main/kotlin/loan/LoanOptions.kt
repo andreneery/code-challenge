92f6d6fc85e7d50664ebf9ce3f2a567d136d287b
André Nery <andre.nery@creditas.com>
2023-05-18 10:09:16 -0300

    ajustando lógica dos emprestimos

--- src/main/kotlin/loan/LoanOptions.kt
+++ src/main/kotlin/loan/LoanOptions.kt
@@ -8,55 +8,28 @@ class LoanOptions(
 
     fun evaluateLoanOptions(): List<Loan> {
         val loans = mutableListOf<Loan>()
+        val personal = Loan(LoanType.PERSONAL, 4.0)
+        val guaranteed = Loan(LoanType.GUARANTEED, 3.0)
+        val consignment = Loan(LoanType.CONSIGNMENT, 2.0)
 
         if (customer.income <= 3000) {
-            loans.add(
-                Loan(
-                    type = LoanType.PERSONAL,
-                    taxes = 4.0
-                )
-            )
+            loans.add(personal)
             if (customer.age < 30 && customer.location == "SP") {
-                loans.add(
-                    Loan(
-                        type = LoanType.GUARANTEED,
-                        taxes = 3.0
-                    )
-                )
+                loans.add(guaranteed)
             }
         } else if (customer.income < 5000) {
-            loans.add(
-                Loan(
-                    type = LoanType.PERSONAL, taxes = 4.0
-                )
-            )
+            loans.add(personal)
             if (customer.location == "SP") {
-                loans.add(
-                    Loan(
-                        type = LoanType.GUARANTEED,
-                        taxes = 3.0
-                    )
-                )
+                loans.add(guaranteed)
             }
         } else {
-            loans.add(
-                Loan(
-                    type = LoanType.PERSONAL,
-                    taxes = 4.0
-                )
-            )
-            loans.add(
-                Loan(
-                    type = LoanType.CONSIGNMENT,
-                    taxes = 2.0
-                )
-            )
+            loans.add(personal)
+            loans.add(consignment)
             if (customer.age < 30) {
-                 loans.add(Loan(
-                    type = LoanType.GUARANTEED,
-                    taxes = 2.0))
+                loans.add(guaranteed.copy(taxes = 2.0))
             }
         }
+
         return loans
     }
 }
